58426b4f05894be40ab91d03dedc543813b0e6f4
Wei Yang <weiyangedward@gmail.com>
2016-07-08 18:03:10 -0500

    init schaeffer

---
+++ makedir/CMakeFiles/SchaefferImplementNotWeighted.dir/DependInfo.cmake
@@ -0,0 +1,23 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/n-z/weiyang4/simons/Wei/software/SchaefferImplementNotWeighted/Clustering.cpp" "/home/n-z/weiyang4/simons/Wei/software/SchaefferImplementNotWeighted/makedir/CMakeFiles/SchaefferImplementNotWeighted.dir/Clustering.cpp.o"
+  "/home/n-z/weiyang4/simons/Wei/software/SchaefferImplementNotWeighted/Orthology.cpp" "/home/n-z/weiyang4/simons/Wei/software/SchaefferImplementNotWeighted/makedir/CMakeFiles/SchaefferImplementNotWeighted.dir/Orthology.cpp.o"
+  "/home/n-z/weiyang4/simons/Wei/software/SchaefferImplementNotWeighted/SpeciesNetwork.cpp" "/home/n-z/weiyang4/simons/Wei/software/SchaefferImplementNotWeighted/makedir/CMakeFiles/SchaefferImplementNotWeighted.dir/SpeciesNetwork.cpp.o"
+  "/home/n-z/weiyang4/simons/Wei/software/SchaefferImplementNotWeighted/main.cpp" "/home/n-z/weiyang4/simons/Wei/software/SchaefferImplementNotWeighted/makedir/CMakeFiles/SchaefferImplementNotWeighted.dir/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ makedir/CMakeFiles/SchaefferImplementNotWeighted.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/SchaefferImplementNotWeighted.dir/main.cpp.o"
+  "CMakeFiles/SchaefferImplementNotWeighted.dir/Clustering.cpp.o"
+  "CMakeFiles/SchaefferImplementNotWeighted.dir/Orthology.cpp.o"
+  "CMakeFiles/SchaefferImplementNotWeighted.dir/SpeciesNetwork.cpp.o"
+  "SchaefferImplementNotWeighted.pdb"
+  "SchaefferImplementNotWeighted"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/SchaefferImplementNotWeighted.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
